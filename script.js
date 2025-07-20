a66c8cdd418376a3206d52235f1009f2954e5a6b
digitalpromot <jawadfsd50@gmail.com>
2025-07-20 11:55:04 +0500

    till the display

---
+++ script.js
@@ -0,0 +1,147 @@
+// let name = "muhammad jawad ali";
+    // let age = 67;
+    // let value = true;
+    // let values = false;
+    // let s;
+    
+    // console.log( name , typeof name);
+    // console.log( age , typeof age);
+    // console.log(value , typeof value );
+    // console.log(values , typeof values);
+    // console.log(s , typeof s);
+    
+    // assignmet opertors 
+//     let a;
+//     a = 10;
+//     a = a + 10;
+//     a += 10 ;
+//     console.log(a);
+//     a = a - 10 ;
+//     a -= 5;
+//     console.log(a);
+//     a = 10 * 60 ;
+//     a *= 60
+//     console.log(a);
+//     a = a / 50 ;
+//     a /= 50 ;
+//     console.log(a);
+//     a = 70000;
+//     console.log(a);
+
+//     a = 10 % 9;
+//    console.log(a);
+//     a = 10 * 3;
+//     console.log(a);
+
+// camparison operators
+// let a = 100 ;
+// let b = '100' ;
+
+// console.log( a == b );
+// console.log ( a === b);
+// console.log( a!==b);
+// console.log( a!= b );
+// console.log ( a < b );
+// console.log( a > b );
+// console.log ( a <= b );
+// console.log ( a >= b );
+
+
+// logical operators
+// let allow = true ;
+// let noAllow = false ;
+
+//         console.log( " you are :" , allow && noAllow );
+//         console.log( " you are :" , allow || noAllow );
+//         console.log( " you are :" , !allow );
+//         console.log( " you are :", noAllow );
+    // long version of the if and else 
+// let marks;
+// marks = 5 ;
+// if (marks > 90){
+//     console.log("You got 'A' Divison ");
+// } else if ( marks > 70 ){
+//     console.log( "you got 'B' Divison");
+// }
+// else if ( marks <= 70 && 33 <= marks )
+// {
+//     console.log(" you are just pass");
+// }
+//  else  {
+//     console.log ( " You need to revise the cource");
+//  }
+
+// shor type of the if and the else with example 
+// condition ? value_if_true : value_if_false;
+
+// check the no is even or the odd
+
+// let number = 2000;
+// let result = number % 2 === 0 ? " even " : "odd";
+// console.log(result);
+
+// // see the adult age
+
+//     let age = 18 ;
+//     let status = age >= 18 ? "adult" : "kid";
+//     console.log(status);
+
+// // see the weather 
+
+// let temprature = 50
+// let weather = temprature > 30 ? " Hot " : " Good ";
+// console.log(weather);
+    
+    // let ageAllow = 25;
+    // let legal = ( ageAllow >= 18 && ageAllow <= 55 ) ? "allow" : "not allowed";
+    // console.log(legal);
+    // see  the how can access this website 
+    // let role = " admin";
+    // let access = ( role === "editor" || role === "admin")
+    // ? "permission granted" : "premission Declined"  ;
+    // console.log(access);
+// see the who is failed and underage
+// let marks = 90;
+// let age = 15 ;
+// let result = ( marks > 33 && age > 16 )? "pass" : "fail";
+// console.log(result);
+
+// let name = "yes";
+// let emptyName = !name ? "name is requied" : "name looks good";
+// console.log(emptyName);
+
+// let isAdmin = "false";
+// let access = !isAdmin ?  "no admin acesss " : " welcome admin ";
+// console.log(access);
+
+// let verified = false;
+// let access = !verified ? "Access Denied" : "Access Granted";
+// console.log(access);
+
+
+// let name = "";
+// let result = !name ? "Name is missing" : "Name is provided";
+// console.log(result);
+
+// const temp = true ;
+// let weather = !temp ? "weather is good" : "weather is bad";
+// document.writeln(weather);
+
+// let name = "jawad";
+// let access = !name ? " access denied " : " welcome Jawad ";
+// document.writeln(access);
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Line", i);
+// }
+//  for (let i = 1; i <= 100; i++ ){
+//     console.log("row", i );
+//  }
+
+   console.log("hello thier");
+
+
+
+
+
+
